071d1f05a56f9d18eda9321e5e8bdcc9285e8b24
Marcus Funch <mfm@magenta.dk>
2025-02-24 18:29:22 +0100

    Gunicorn: Switch to gthread to better handle IO, restart workers less frequently

--- docker/gunicorn-settings.py
+++ docker/gunicorn-settings.py
@@ -4,12 +4,18 @@
 # Used by Dockerfile - compose does a few overrides
 import multiprocessing
 
+num_workers = multiprocessing.cpu_count() * 3
+
+worker_class = "gthread"
 bind = "0.0.0.0:9999"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = num_workers
+threads = num_workers * 4
 accesslog = "-"
 errorlog = "-"
 worker_tmp_dir = "/dev/shm"
-max_requests = 1000
+# To help reduce potential memory leaks
+max_requests = 1500
+# To reduce the risk of many workers restarting simultaneously
 max_requests_jitter = 50
 # The IP of the traefik container - if not specified, gunicorn
 # only trusts x forwarded for coming from 127.0.0.1

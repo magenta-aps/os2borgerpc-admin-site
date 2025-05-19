a2aa1e8a9302ff227a81c2a31a7766005a12f376
Lise Petculescu <lfjp@magenta-aps.dk>
2025-05-19 15:12:39 +0200

    replacing hardcoded values with db values

--- admin_site/system/views_dir/pc_group_management_view.py
+++ admin_site/system/views_dir/pc_group_management_view.py
@@ -3,28 +3,41 @@
 from django.urls import reverse
 
 from django.views.generic import RedirectView
+
+from system.models import SettingsCategory, SubSettingsCategory
 from system.views_dir.utils.helper_views import SiteView
 from system.mixins.views_mixins import SiteMixin
+from django.urls import reverse
+from django.views.generic.base import RedirectView
 
 
 class SettingsCategoriesRedirect(RedirectView, SiteMixin):
     def get_redirect_url(self, *args, **kwargs):
-        # chosen_category = modelName.objects.getFirst()
+        first_category = SettingsCategory.objects.order_by('title').first()
+
+        if not first_category:
+            return "/"  # fallback if empty
+
         return reverse(
-            "pc_setting_category",
-            kwargs={"slug": kwargs["slug"], "category": "browser"},
+            "pc_setting_category",  # your url name
+            kwargs={
+                "slug": kwargs["slug"],                 # e.g., "magenta"
+                "category": first_category.title,       # e.g., "Browser"
+            },
         )
 
 class SettingsCategories(SiteView, SiteMixin):
-    template_name = "system/pc_settings_page/settings_list.html"
+    template_name = "system/pc_management_page/settings_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        context["categories"] = SettingsCategory.objects.all()
+        context["sub_category"] = SubSettingsCategory.objects.filter()
+        context["slug"] = self.kwargs["slug"]
         return context
 
 class SettingsCategoriesSpecific(SiteView, SiteMixin):
-    template_name = "system/pc_settings_page/settings_detail_page.html"
+    template_name = "system/pc_management_page/settings_detail_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

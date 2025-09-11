51191f71d25f9d40b9ae0dc86c4fa263bad58414
Marcus Funch <mfm@magenta.dk>
2025-09-11 18:55:47 +0200

    Remove django admin docs and an unused application

--- admin_site/os2borgerpc_admin/settings.py
+++ admin_site/os2borgerpc_admin/settings.py
@@ -245,9 +245,6 @@ DJANGO_APPS = (
     "django.contrib.staticfiles",
     # Uncomment the next line to enable the admin:
     "django.contrib.admin",
-    # Uncomment the next line to enable admin documentation:
-    "django.contrib.admindocs",
-    "django.forms",
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

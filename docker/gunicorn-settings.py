68204d6b7178696870160b32eaac383d38367c52
Marcus Funch <mfm@magenta.dk>
2026-04-27 11:02:48 +0200

    Gunicorn: Add documentation/references

--- docker/gunicorn-settings.py
+++ docker/gunicorn-settings.py
@@ -8,6 +8,9 @@ num_workers = multiprocessing.cpu_count() * 3
 
 worker_class = "gthread"
 bind = "0.0.0.0:9999"
+# The recommendations in regards to both workers and threads are:
+# "A positive integer generally in the 2-4 x $(NUM_CORES) range. You’ll want to vary this a bit to find the best for your particular application’s work load."
+# https://gunicorn.org/reference/settings/?h=settings#worker-processes
 workers = num_workers
 threads = num_workers * 4
 accesslog = "-"
